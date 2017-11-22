3a719a5d695d10bfa75c639bdcd97053a0287570
danielarevalo100 <danielarevalo100@gmail.com>
2017-11-22 07:10:17 -0400

    al fin

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+
+import java.io.IOException;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -13,7 +16,7 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         ventana a = new ventana();
     }
     
